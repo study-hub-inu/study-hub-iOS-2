5e12f806653b24eb9d58cf57c423bd20f885c4d5
rrheon <yongheon0806@gmail.com>
2023-11-24 16:05:55 +0900

    feat - 참여자확인 VC 구현 중

--- STUDYHUB2/Extension/UIColor+.swift
+++ STUDYHUB2/Extension/UIColor+.swift
@@ -26,6 +26,8 @@ extension UIColor {
   static let g100 = UIColor(red: 0.21, green: 0.21, blue: 0.21, alpha: 1.00)
   static let g10 = UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.00)
   static let g60 = UIColor(red: 0.56, green: 0.56, blue: 0.56, alpha: 1.00)
+  
+  static let g_10 = UIColor(red: 0.16, green: 0.82, blue: 0.18, alpha: 1.00)
 
   static let changeInfo = UIColor(red: 1.00, green: 0.33, blue: 0.19, alpha: 1.00)
 
